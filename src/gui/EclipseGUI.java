efb8c1de18dcc7b000ce5fd33c4aea35da07912d
BryanGF0822 <bryanicesi0822@gmail.com>
2020-11-27 19:39:09 -0500

    It is already possible that it will change color when the eclipse begins.

--- src/gui/EclipseGUI.java
+++ src/gui/EclipseGUI.java
@@ -3,11 +3,20 @@ package gui;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import model.Moon;
 
 public class EclipseGUI {
+	
+    private final static int[] ILLUMINATED = new int[] {255, 249, 139};
+    
+    @FXML
+    private Pane sky;
+
+    @FXML
+    private Circle sun;
 
     @FXML
     private Circle circleShape;
@@ -40,6 +49,17 @@ public class EclipseGUI {
     				Platform.runLater(new Thread() {
     					public void run() {
     						updateMoon(mn.getX());
+    						
+    						double diff = Math.abs(sun.getLayoutX()-circleShape.getLayoutX());
+    				    	double diameter = circleShape.getRadius()*2;
+    				    	if(diff<=diameter) {
+    				    		double pos = diff/diameter;
+    				    		int r = (int)(ILLUMINATED[0]*pos);
+    				    		int g = (int)(ILLUMINATED[1]*pos);
+    				    		int b = (int)(ILLUMINATED[2]*pos);
+    				    		sky.setStyle("-fx-background-color:rgb("+r+","+g+","+b+");");
+    				    	}  
+    						
     					}
     				});
     				
@@ -69,6 +89,16 @@ public class EclipseGUI {
 //    							circleShape.relocate(646, 92);
 //    						}
     						updateMoon(mn.getX());
+    						
+    						double diff = Math.abs(sun.getLayoutX()-circleShape.getLayoutX());
+    				    	double diameter = circleShape.getRadius()*2;
+    				    	if(diff<=diameter) {
+    				    		double pos = diff/diameter;
+    				    		int r = (int)(ILLUMINATED[0]*pos);
+    				    		int g = (int)(ILLUMINATED[1]*pos);
+    				    		int b = (int)(ILLUMINATED[2]*pos);
+    				    		sky.setStyle("-fx-background-color:rgb("+r+","+g+","+b+");");
+    				    	}  
     					}
     				});
     				try {
